529b3c84a697bc4b301a30365473be4df97d086f
Jason Blocklove <jason.blocklove@gmail.com>
2022-04-18 19:47:40 -0400

    Gotta increase that github score

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 wget -qO- https://github.com/JBlocklove/remarkable-daily-pdf/archive/dev.zip | unzip -
 mv remarkable-daily-pdf-dev remarkable-daily-pdf
 
-echo -n "Do you want this to run automatically every day? [y/N]"
+echo -n "Do you want this to run automatically every day? [y/N] "
 read auto
 if [[ $auto == y ]]; then
 	cp -v crossword.service crossword.timer /etc/systemd/system/
