8dbee316dba518884b9c6b59547ee69362eff15b
Jason Blocklove <jason.blocklove@gmail.com>
2022-04-18 19:48:12 -0400

    Gotta increase that github score

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 wget -qO- https://github.com/JBlocklove/remarkable-daily-pdf/archive/dev.zip | unzip -
 mv remarkable-daily-pdf-dev remarkable-daily-pdf
+cd remarkable-daily-pdf
 
 echo -n "Do you want this to run automatically every day? [y/N] "
 read auto
